cffeb0e8be12bc0f1254037de9753a842250d668
Ji-Hwan Kim <ji-hwankim@Ji-Hwanui-Mac-Studio.local>
2023-08-01 22:18:37 +0900

    [Script push]

--- src/main/kotlin/studio/hcmc/board/dto/ErrorDTO.kt
+++ src/main/kotlin/studio/hcmc/board/dto/ErrorDTO.kt
@@ -3,7 +3,7 @@ package studio.hcmc.board.dto
 sealed interface ErrorDTO {
     val throwable: Throwable
 
-    data object BoardNotFound : Throwable(), ErrorDTO {
+    data object BoardNotFound : RuntimeException(), ErrorDTO {
         override val throwable get() = this
 
         private fun readResolve(): Any = BoardNotFound
@@ -15,7 +15,19 @@ sealed interface ErrorDTO {
         private fun readResolve(): Any = ArticleNotFound
     }
 
+    data object ArticlePasswordMismatch : RuntimeException(), ErrorDTO {
+        override val throwable: Throwable get() = this
+
+        private fun readResolve(): Any = ArticlePasswordMismatch
+    }
+
     data object CommentNotFound : RuntimeException(), ErrorDTO {
+        override val throwable get() = this
+
+        private fun readResolve(): Any = CommentNotFound
+    }
+
+    data object CommentPasswordMismatch : RuntimeException(), ErrorDTO {
         override val throwable get() = this
 
         private fun readResolve(): Any = CommentNotFound
